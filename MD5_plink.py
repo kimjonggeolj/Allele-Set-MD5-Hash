75aeb68afe313fc341d25ca461ebfe871491c410
kimjonggeolj <kim.jonggeol.j@gmail.com>
2020-05-28 14:48:40 -0400

    Revert "Update MD5_plink.py"
    This reverts commit 60c7829925c48d9e88db97f4da099c2606543cda.

--- MD5_plink.py
+++ MD5_plink.py
@@ -4,14 +4,16 @@ import subprocess
 
 class MD5_plink():
     # === Following are functions for generating a string list that can be hashed ===
-    def traw_gen(input_name, output_name = "traw_temp", allele_list = 'forenseq.hg19.set'):
+    def traw_gen(input_name, output_name = None, sampleID_file, allele_list = 'forenseq.hg19.set'):
         """generates traw from PLINK using `--export A-transpose`"""
+        if output_name == None:
+            output_name = input_name + "_traw"
         # traw is truncated to just allele_list
-        bashCommand = "plink --bfile " + input_name + " --extract range " + allele_list + " --export A-transpose --out " + output_name
+        bashCommand = "plink --bfile " + input_name + " --keep " + sampleID_file + " --extract range " + allele_list + " --export A-transpose --out " + output_name
         subprocess.run(bashCommand.split(), stdout=subprocess.PIPE)
         
-    def allele_string_traw_gen(traw, sampleID, allele_list = 'forenseq.hg19.set'):
-        """this will convert traw from PLINK `--export A-transpose` to a single string of alleles. It must be given a gene set file used to generate the traw."""
+    def allele_string_traw_gen(traw, allele_list = 'forenseq.hg19.set', ref_list):
+    """this will convert traw from PLINK `--export A-transpose` to a single string of alleles. It must be given a gene set file used to generate the traw."""
         # import from traw
         traw = pd.read_csv(traw, sep = '\t')
         # start with blank string, add as it goes
@@ -20,16 +22,15 @@ class MD5_plink():
         forenseq_list = pd.read_csv(allele_list, sep = '\t', names = ['CHR', 'POS', 'remove1', 'remove2'], usecols = ['CHR', 'POS'])
         #   If there are different number of alleles in traw file (presumably truncated to same allele list) and forenseq
         #   ...prepare the warning message because there are alleles missing!
-        sampleColumn = traw.columns.get_loc(sampleID)
         if len(traw) != len(forenseq_list):
             traw_merged = forenseq_list.merge(traw.drop(columns = 'CHR'), how = "outer", on = "POS")
             warning_alleles = ""
             for index, row in traw_merged.fillna(-1).iterrows():
-                if row[sampleColumn] == 0:
+                if row[6] == 0:
                     allele = row[5]+row[5]
-                elif row[sampleColumn] == 1:
+                elif row[6] == 1:
                     allele = row[4]+row[5]
-                elif row[sampleColumn] == -1:
+                elif row[6] == -1:
                     allele = "NN"
                     warning_alleles = warning_alleles+" "+str(row[0])+":"+str(row[1])
                 else:
@@ -40,9 +41,9 @@ class MD5_plink():
         #   If the number of alleles are identical, then no warning message
         else:
             for index, row in traw.iterrows():
-                if row[sampleColumn] == 0:
+                if row[6] == 0:
                     allele = row[5]+row[5]
-                elif row[sampleColumn] == 1:
+                elif row[6] == 1:
                     allele = row[4]+row[5]
                 else:
                     allele = row[4]+row[4]
@@ -51,18 +52,11 @@ class MD5_plink():
             return string_traw
     # Function that just tapes the two previous functions together
     # presumably what will happen is: plug the output of this function into MD5 function
-    def allele_string_gen(input_name, allele_list = 'forenseq.hg19.set'):
+    def allele_string_gen(input_name, sampleID_file, allele_list = 'forenseq.hg19.set'):
         # generate temp traw
-        MD5_plink.traw_gen(input_name = input_name)
+        traw_gen(input_name = input_name, output_name = "traw_temp", sampleID_file = sampleID_file)
         # take the traw and generate allele string
-        df=pd.DataFrame(columns = ["sampleID", "allele_list"])
-        sampleID=pd.read_csv(input_name+".fam", sep="\s", header=None, names=["FID","IID","r1","r2","r3","r4"], usecols=["FID","IID"])
-        sampleID=sampleID['FID']+"_"+sampleID['IID']
-        # instead of "allele_list" it would be "hash" column instead in the final script
-        for i in sampleID:
-            new_row={'sampleID':i, "allele_list":MD5_plink.allele_string_traw_gen(traw = "traw_temp.traw", sampleID = i)}
-            df=df.append(new_row, ignore_index=True)
+        allele_string_traw_gen(traw = "traw_temp.traw")
         # clean up temp
         os.remove("traw_temp.log")
         os.remove("traw_temp.traw")
-        return df
